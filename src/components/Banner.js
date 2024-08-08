ae0be27f843ea03695bbdc9f600be7693c4af723
Leandro de Souza Cunha <leandro.kate@gmail.com>
2024-08-08 14:13:55 -0300

    Adicionando animação Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,31 +7,82 @@ import { fadeIn } from '../variants'
 
 const Banner = () => {
   return (
-    <section id='home' className='section'>
+    <section id='home' className='min-h-[85vh] lg:min-h-[78vh] flex items-center'>
       <div className='container mx-auto'>
-        {/* { TEXTO } */}
-        <div>
-          <h1>
-            Leandro <span>Cunha</span>
-          </h1>
-          <div className='mb-6 text-[36px] lg:text[60px] font-secondary font-semibold uppercase leading-[1]'>
-            <span className='text-white mr-4'>Eu sou um</span>
-            <TypeAnimation sequence={[
-              'Developer', 2000,
-              'Designer', 2000,
-              'Dad', 2000,
-            ]}
-            speed={50} 
-            className='text-accent'
-            wrapper='span'
-            repeat={Infinity}
-            />
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'> 
+          {/* { TEXTO } */}
+          <div className='flex-1 text-center font-secundary lg:text-left'>
+            <motion.h1 
+            variants={fadeIn('up', 0.3)} 
+            initial="hidden" 
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.7}}
+            className='text-[50px] font-bold leading-[0.8] lg:text-[110px]'>
+              Leandro <span>Cunha</span>
+            </motion.h1>
+            <motion.div 
+              variants={fadeIn('up', 0.4)} 
+              initial="hidden" 
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='mb-6 text-[36px] lg:text[60px] font-secondary font-semibold uppercase leading-[1]'>
+                <span className='text-white mr-4'>Eu sou um</span>
+                <TypeAnimation sequence={[
+                  'Developer', 2000,
+                  'Designer', 2000,
+                  'Dad', 2000,
+                ]}
+                speed={50} 
+                className='text-accent'
+                wrapper='span'
+                repeat={Infinity}
+                />
+            </motion.div>
+            <motion.p 
+              variants={fadeIn('up', 0.5)} 
+              initial="hidden" 
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='mb-8 max-x-lg mx-auto lg:mx-0'>
+              Estou em uma transição de carreira de Especialista em Infraestrutura de TI para Desenvolvedor FullStack, conheço muito afundo sobre servidores e Web e estou adquirindo conhecimento em Programação para essa transição!
+            </motion.p>
+            <motion.div 
+              variants={fadeIn('up', 0.6)} 
+              initial="hidden" 
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+              <button className='btn btn-lg'>Entre em contato</button>
+              <a href='#' className='text-gradient btn-link' >Meu Portfólio</a>
+            </motion.div>
+            {/* { LINKS } */}
+            <motion.div 
+              variants={fadeIn('up', 0.7)} 
+              initial="hidden" 
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+              <a href='#'>
+                <FaGithub />
+              </a>
+              <a href='#'>
+                <FaLinkedin />
+              </a>
+              <a href='#'>
+                <FaInstagram />
+              </a>
+            </motion.div>
           </div>
         </div>
+        
         {/* { IMAGEM } */}
-        <div>
+        <motion.div 
+          variants={fadeIn('down', 0.5)} 
+          initial="hidden" 
+          whileInView={'show'}
+          className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[480px]'>
           <img src={Image} alt='' />
-        </div>
+        </motion.div>
       </div>
     </section>
   ); 
