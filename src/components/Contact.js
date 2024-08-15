231568c3d0e328c1a0e1a0c62d4440688abf531c
Leandro de Souza Cunha <leandro.kate@gmail.com>
2024-08-15 14:25:47 -0300

    Adicionando código envio email Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import { useState } from 'react'
 import { motion } from "framer-motion";
 import { fadeIn } from '../variants'
 
 const Contact = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    if(name === '' || email === '' || message === '') {
+      alert("Ops! Preencha todos os campos!");
+      return;
+    }
+
+    alert("Teste")
+  }
+
   return (
     <section id='contact' className='lg:section py-16'>
       <div className='container mx-auto'>
@@ -26,19 +43,26 @@ const Contact = () => {
             initial="hidden"
             whileInView={"show"}
             viewport={{once: false, amount: 0.3}}
-            className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start'>
+            className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start' onSubmit={sendEmail}>
             <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all'
             type='text'
             placeholder='Seu nome'
+            onChange={(e) => setName(e.target.value)}
+            value={name}
             />
             <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all'
-            type='text'
+            type='email'
             placeholder='Seu email'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             />
             <textarea className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12'
+            type='textarea'
             placeholder='Sua mensagem'
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
             ></textarea>
-            <button className='btn btn-lg'>
+            <button className='btn btn-lg' type="submit">
               Enviar mensagem
             </button>
           </motion.form>
