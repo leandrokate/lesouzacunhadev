b7a0abcfee994d0c4183f840b099d41c78588130
Leandro de Souza Cunha <leandro.kate@gmail.com>
2024-08-16 06:06:40 -0300

    Finalizado ajuste de cores no CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
     extend: {
       colors: {
         primary: '#0a0a0a',
-        accent: '#B809C3',
+        accent: '#00AFEF',
       },
       backgroundImage: {
         site: "url('./assets/site-bg.jpg')",
